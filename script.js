49f3f1c710ca94bb6468ccede9f582d052becb8f
kjbetts <kjbetts@github.com>
2018-02-06 12:41:39 -0500

    Master

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-$( document ).ready(function() {
-    $(".table").on("click", function(){
-      $(".reservations").toggleClass("show")
-    })
-    
-})
+$(document).ready(function() {
+    var table;
+  $(".table").on("click", function() {
+    $(".reservations").toggleClass("show")
+     table= $(event.target);
+  })
+  $("#Save").on("click", function() {
+    table.addClass('selected');
+  $(".reservations").toggleClass("show")
+  })
+
+
+
+
+
+});
